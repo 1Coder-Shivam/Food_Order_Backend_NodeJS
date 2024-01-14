4434a9b6ed2a50e2b458068c6a71bb12c905cad4
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2024-01-14 20:25:37 +0530

    Shopping- All ops

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response, NextFunction } from 'express'
-import { Vandor } from '../models';
+import { FoodDoc, Vandor } from '../models';
 
 export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction)=>{
     const pincode = req.params.pincode;
-    const result = await Vandor.find({pincode: pincode})
+    const result = await Vandor.find({pincode: pincode, serviceAvailable: false})
     .sort([['rating','descending']])
     .populate('foods')
 
@@ -17,17 +17,67 @@ export const GetFoodAvailability = async (req: Request, res: Response, next: Nex
 }
 
 export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction)=>{
+    const pincode = req.params.pincode;
+    const result = await Vandor.find({pincode: pincode, serviceAvailable: false})
+    .sort([['rating','descending']])
+    .limit(1)
 
+    if(result.length>0){
+        return res.status(200).json(result);
+    }
+
+    return res.status(404).json({
+        message: "No restaurants found"
+    })
 }
 
 export const GetFastFoods = async (req: Request, res: Response, next: NextFunction)=>{
+    const pincode = req.params.pincode;
+    const result = await Vandor.find({pincode: pincode, serviceAvailable: false})
+    .populate('foods')
 
+    if(result.length>0){
+        let foodResult: any = []
+        result.map((vandor)=>{
+            const foods = vandor.foods as [FoodDoc]
+            
+            foodResult.push(...foods.filter(food=> food.readyTime<=30));
+
+        })
+        return res.status(200).json(foodResult);
+    }
+
+    return res.status(404).json({
+        message: "No Data found"
+    })
 }
 
 export const SearchFoods = async (req: Request, res: Response, next: NextFunction)=>{
+    const pincode = req.params.pincode;
+    const result = await Vandor.find({pincode: pincode, serviceAvailable: false})
+    .populate('foods')
 
+    if(result.length>0){
+        let foodResult: any = []
+        result.map(item => foodResult.push(...item.foods) )
+        return res.status(200).json(foodResult);
+    }
+
+    return res.status(404).json({
+        message: "No Data found"
+    })
 }
 
 export const RestaurantById = async (req: Request, res: Response, next: NextFunction)=>{
+    const id = req.params.id;
+    const result = await Vandor.findById(id).populate('foods')
+    
 
+    if(result){
+        return res.status(200).json(result);
+    }
+
+    return res.status(404).json({
+        message: "No restaurants found"
+    })
 }
